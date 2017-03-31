a4828abddf45a92e190cc248638e679ad67c3fc8
bb <biletskyboh@gmail.com>
2017-03-31 18:02:26 +0300

    created models

--- test_pj/notes/models.py
+++ test_pj/notes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Notes(models.Model):
+    name = models.CharField(max_length=128)
+    body = models.CharField(max_length=1024)
+    pub_date = models.DateTimeField(default=timezone.datetime.now)
+
+
