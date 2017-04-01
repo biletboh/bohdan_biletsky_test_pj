30fed265a8f0ed2897d556fbd56bbd541c8f0644
bb <biletskyboh@gmail.com>
2017-04-01 21:50:41 +0300

    added update views, templates, urls

--- notes/urls.py
+++ notes/urls.py
@@ -7,6 +7,10 @@ app_name='notes'
 
 
 urlpatterns = [
-        url(r'^$', views.NotesList.as_view(), name = 'notes list'),
-        url(r'create/$', views.CreateNotes.as_view(), name = 'create notes'),
+        url(r'^$', views.NotesList.as_view(), name = 'notes_list'),
+        url(r'create/$', views.CreateNotes.as_view(), 
+            name = 'create_notes'),
+        url(r'(?P<pk>[0-9]+)/update/$', views.UpdateNotes.as_view(), 
+            name = 'update_notes'),
+        url(r'(?P<pk>[0-9]+)/delete/$', views.CreateNotes.as_view(), name = 'delete_notes'),
         ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import FormView 
+from django.views.generic import DeleteView 
+from django.urls import reverse_lazy
 from notes.models import Notes
 from notes.forms import NotesForm
 
@@ -17,4 +19,28 @@ class CreateNotes(FormView):
     def form_valid(self, form):
         note = Notes.objects.create(name=form.cleaned_data['name'], body=form.cleaned_data['body'])
         return super(CreateNotes, self).form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super(CreateNotes, self).get_context_data(**kwargs)
+        context['title'] = 'Create' 
+        return context
 
+
+class UpdateNotes(FormView):
+    template_name = 'notes/create_notes.html'
+    form_class = NotesForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        note = Notes.objects.update(name=form.cleaned_data['name'], body=form.cleaned_data['body'])
+        return super(UpdateNotes, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdateNotes, self).get_context_data(**kwargs)
+        context['title'] = 'Update' 
+        return context
+
+
+class DeleteNotes(DeleteView):
+    model = Notes
+    success_url = reverse_lazy('notes_list')
