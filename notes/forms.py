7e50ed9481f2ac945a11d94ff7edb00c62623ae0
bb <biletskyboh@gmail.com>
2017-04-01 14:47:23 +0300

    fixed forms

--- notes/forms.py
+++ notes/forms.py
@@ -3,5 +3,5 @@ from notes.models import Notes
 
 
 class NotesForm(forms.Form):
-    name = forms.CharField(label='title', min_length=10, max_length=128)
-    body = forms.CharField(label='content', min_length=10, max_length=1024)
+    name = forms.CharField(required=True, label='title', min_length=10, max_length=128)
+    body = forms.CharField(required=True, label='content', min_length=10, max_length=1024)
