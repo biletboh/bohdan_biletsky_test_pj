64a8985b4113cb6847cd81c9d485b2bf5366fec9
bb <biletskyboh@gmail.com>
2017-04-01 15:25:32 +0300

    added textarea widget

--- notes/forms.py
+++ notes/forms.py
@@ -4,4 +4,4 @@ from notes.models import Notes
 
 class NotesForm(forms.Form):
     name = forms.CharField(required=True, label='title', min_length=10, max_length=128)
-    body = forms.CharField(required=True, label='content', min_length=10, max_length=1024)
+    body = forms.CharField(required=True, label='content', min_length=10, max_length=1024, widget=forms.Textarea)
