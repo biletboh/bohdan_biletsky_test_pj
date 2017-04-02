efda2b672ef7fb0506b1148b9e5d2df75228272c
bb <biletskyboh@gmail.com>
2017-04-02 18:59:33 +0300

    added custom context_processor to show count of all notes

---
+++ notes/context_processors.py
@@ -0,0 +1,5 @@
+from notes.models import Notes
+
+def notes_count_processor(request):
+    notes = Notes.objects.all()            
+    return {'all_notes': notes.count()}
--- test_pj/settings.py
+++ test_pj/settings.py
@@ -54,6 +54,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'notes.context_processors.notes_count_processor',
             ],
         },
     },
