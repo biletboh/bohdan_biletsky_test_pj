1186a84a7bd0fb107c8bddd559a96d0c0693a585
bb <biletskyboh@gmail.com>
2017-04-05 16:00:57 +0300

    added styles to the widget

--- notes/static/notes/js/widget.js
+++ notes/static/notes/js/widget.js
@@ -15,6 +15,7 @@
           '<div class="notes"><div class="row"> <div class="col-md-12"><h3><strong>'+ random_note.fields.name +'</strong></h3></div> </div> <hr> <div class="row"> <div class="col-md-12"> <p>'+ random_note.fields.body +'</p></div></div></div>'
 
           );
+        $('.notes').css({'background': '#DFDEDE', 'border-radius': '4px', 'box-shadow': '0 1px 2px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(63, 63, 68, 0.1)', 'padding': '10px', 'margin': '20px 5px'});
       });
     });
   }
