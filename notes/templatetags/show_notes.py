1d4346634e24f575ff6f3368ee3455c0d705ceee
bb <biletskyboh@gmail.com>
2017-04-06 22:02:20 +0300

    deleted print message

--- notes/templatetags/show_notes.py
+++ notes/templatetags/show_notes.py
@@ -8,5 +8,5 @@ register = template.Library()
 @register.inclusion_tag('notes/notes.html')
 def show_notes(note_id):
     note = Notes.objects.get(pk=note_id)
-    print(note, note_id)
     return {'note': note}
+
