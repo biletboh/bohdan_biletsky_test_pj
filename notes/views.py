8083430243c652eede11a4dee685bedae2804802
bb <biletskyboh@gmail.com>
2017-04-08 15:41:28 +0300

    added test for templatetags, fixed pep in views

--- notes/views.py
+++ notes/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from notes.models import Notes
 
+
 class NotesList(ListView):
+    """View that renders list of notes."""
+
     model = Notes 
     template_name = 'notes/notes_list.html'
+
