6db4b808e05af4b6532099c11c362f8a3f0eca4e
bb <biletskyboh@gmail.com>
2017-04-08 08:27:40 +0300

    removed .all() from context processor

--- notes/context_processors.py
+++ notes/context_processors.py
@@ -4,6 +4,6 @@ from notes.models import Notes
 def notes_count_processor(request):
     """Render the count of all notes."""
 
-    notes = Notes.objects.all()            
+    notes = Notes.objects            
     return {'all_notes': notes.count()}
 
