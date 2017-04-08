2de45ebc832ceabee00118ef641262ff9c6604c8
bb <biletskyboh@gmail.com>
2017-04-08 16:05:12 +0300

    moved test to another file

--- notes/forms.py
+++ notes/forms.py
@@ -2,20 +2,10 @@ from django import forms
 from notes.models import Notes
 
 
-class CustomUpperCaseField(forms.CharField):
-    """Custom CharField that transform value to upper case."""
-
-    def clean(self, value):
-        try:
-            return value.upper()
-        except:
-            raise ValidationError
-
-
 class NotesForm(forms.Form):
     """The form that handles user input for a note."""
 
-    name = CustomUpperCaseField(
+    name = forms.CharField(
         required=True, label='title', min_length=10, max_length=128)
     body = forms.CharField(
         required=True, label='content', min_length=10, max_length=1024,
